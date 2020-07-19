5891cbfb48e14524ce6f4e1a96d6ea75c76bb98d
subhamManU <subhamsaha90@gmail.com>
2020-07-20 01:43:23 +0530

    Deployment related changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/portfolioUIApp'));
+app.use(express.static(__dirname + '/dist/EasyFindApp'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/portfolioUIApp/index.html'));
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/EasyFindApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
