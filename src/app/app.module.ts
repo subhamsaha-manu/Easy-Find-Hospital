9f7ce1fc8d278429599d79f0183264858cfaea42
subhamManU <subhamsaha90@gmail.com>
2020-07-23 23:11:34 +0530

    Fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-<<<<<<< HEAD
-import { FlexLayoutModule } from "@angular/flex-layout";
-=======
 
 import {FlexLayoutModule} from '@angular/flex-layout';
->>>>>>> deployment
 
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
